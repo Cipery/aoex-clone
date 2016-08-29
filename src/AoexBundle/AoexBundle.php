cbeb415ee4fb544a66d4d6875d9f558e6e872d62
Cipery <daniel.cipra@gmail.com>
2016-08-30 00:15:23 +0200

    added AoexBundle, removed AppBundle

---
+++ src/AoexBundle/AoexBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 30. 8. 2016
+ * Time: 0:10
+ */
+
+namespace AoexBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AoexBundle extends Bundle  {
+
+}
